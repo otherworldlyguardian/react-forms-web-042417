c4aa058eeb41c1b775c776b8fd38177841a89d90
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-06-27 17:54:32 -0400

    readme marked as lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import ControlledInput from './components/ControlledInput'
 
 ReactDOM.render(
   <div>Mount Components Here</div>,
